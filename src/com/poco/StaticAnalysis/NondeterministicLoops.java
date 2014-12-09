4b33d468fb26a6e8b70086b7a7391a30b57eb1a8
danid0729 <dferguson@cse.usf.edu>
2014-12-08 19:53:06 -0500

    Finish nondeterministic loops

--- src/com/poco/StaticAnalysis/NondeterministicLoops.java
+++ src/com/poco/StaticAnalysis/NondeterministicLoops.java
@@ -4,10 +4,7 @@ import com.poco.PoCoParser.PoCoParserBaseListener;
 import dk.brics.automaton.Automaton;
 import dk.brics.automaton.RegExp;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Danielle on 8/5/2014.
@@ -20,17 +17,19 @@ public class NondeterministicLoops extends PoCoParserBaseListener {
     String errorsegment1 = "";
     String errorsegment2 = "";
     LinkedHashSet<String> possibleinputs;
+    Map<String, String> bindings;
 
     public NondeterministicLoops(PoCoParser parser) {
         this.parser = parser;
         this.start = new ArrayList<String>();
         this.possibleinputs = possibleinputs;
+        this.bindings = new HashMap<String, String>();
     }
     @Override
     public void exitPolicy(PoCoParser.PolicyContext ctx) {
         NondeterministicLoops(start);
         if(finalresult) {
-            System.out.println("Warning: The loop '" + errorsegment1 + "' followed by '" + errorsegment1 + "' is nondeterministic. This may cause unexpected behavior.");
+            System.out.println("Warning: The loop '" + errorsegment1 + "' followed by '" + errorsegment2 + "' is nondeterministic. This may cause unexpected behavior.");
         }
     }
 
@@ -208,7 +207,7 @@ public class NondeterministicLoops extends PoCoParserBaseListener {
             }
         }
         nextre = nextre.substring(0,nextre.length()-1);
-        nextsegmentstring = segmentstring.substring(0,segmentstring.length()-1);
+        nextsegmentstring = nextsegmentstring.substring(0,nextsegmentstring.length()-1);
 
         re = re.replace("%", ".*");
         RegExp r1 = new RegExp(re);
@@ -284,13 +283,7 @@ public class NondeterministicLoops extends PoCoParserBaseListener {
             start.add("_");
         }
     }
-    @Override
-    public void exitIre(PoCoParser.IreContext ctx) {
-        //only dealing with actual function calls now - TODO: expand to more types
-        if(ctx.re().size() > 0) {
-            start.add(ctx.re().get(0).getText());
-        }
-    }
+
     @Override
     public void enterExecution(PoCoParser.ExecutionContext ctx) {
         if(ctx.children.get(0).getText().equals("(")) {
@@ -309,32 +302,211 @@ public class NondeterministicLoops extends PoCoParserBaseListener {
             start.add("PLUS");
         }
         else if(ctx.children.size() == 2 && ctx.execution().size() == 2) {
-            int index = start.size();
-            if(start.get(start.size()-1) == "END GROUP" || (start.size() > 2 && (start.get(start.size()-2) == "CONCAT" || start.get(start.size()-2) == "BAR"))) {
-                for(int j = start.size()-1; j >0; j--)
+            int index = 1;
+            for(int j = start.size() - 2; j > 0; j--)
+            {
+                if((!start.get(j+1).equals("BAR") && !start.get(j+1).equals("CONCAT"))
+                        && (!start.get(j).equals("BAR") && !start.get(j).equals("CONCAT")))
                 {
-                    if(start.get(j) == "START GROUP")
-                    {
-                        index=j+1;
-                        break;
-                    }
+                    index=j+1;
+                    break;
                 }
             }
-            start.add(index - 1, "CONCAT");
+            start.add(index, "CONCAT");
         }
         else if(ctx.children.size() == 3 && ctx.BAR() != null) {
-            int index = start.size();
-            if(start.get(start.size()-1) == "END GROUP" || (start.size() > 2 && (start.get(start.size()-2) == "CONCAT" || start.get(start.size()-2) == "BAR"))) {
-                for(int j = start.size()-1; j >0; j--)
+            int index = 1;
+            for(int j = start.size() - 2; j > 0; j--)
+            {
+                if((!start.get(j+1).equals("BAR") && !start.get(j+1).equals("CONCAT"))
+                        && (!start.get(j).equals("BAR") && !start.get(j).equals("CONCAT")))
                 {
-                    if(start.get(j) == "START GROUP")
-                    {
-                        index=j+1;
-                        break;
+                    index=j+1;
+                    break;
+                }
+            }
+            start.add(index, "BAR");
+        }
+        else if(ctx.exch() != null && ctx.exch().matchs() != null)
+        {
+            PoCoParser.MatchsContext matchs = ctx.exch().matchs();
+            String re = "";
+            if(matchs.match() != null) {
+                if(matchs.match().ire() != null) {
+                    if (matchs.match().ire().re().size() > 0) {
+                        //The first re is always the action
+                        PoCoParser.ReContext rectx =  matchs.match().ire().re().get(0);
+                        while(rectx.AT() != null)
+                        {
+                            rectx = rectx.re().get(0);
+                        }
+                        if(rectx.DOLLAR() != null)
+                        {
+                            re = replaceValues(rectx);
+                        }
+                        else {
+                            re = rectx.getText();
+                        }
+                    } else {
+                        start.add(".*");
+                        return;
                     }
                 }
             }
-            start.add(index - 1, "BAR");
+            re = re.split("\\(")[0];
+            re = re.replace("%", ".*");
+            start.add(re);
         }
+    }
+
+
+    private String replaceBinding(PoCoParser.ReContext ctx)
+    {
+        String value = ctx.getText();
+
+        //standard variable (not function)
+        if(ctx.DOLLAR() != null) {
+            if (bindings.containsKey(ctx.qid().getText())) {
+                if (ctx.LPAREN() == null) {
+                    value = value.replace("$" + ctx.qid().getText(), bindings.get(ctx.qid().getText()));
+                } else {
+                    PoCoParser.OpparamlistContext opctx = ctx.opparamlist();
+                    List<String> res = new ArrayList<String>();
+                    while (opctx != null) {
+                        if (opctx.re() != null) {
+                            String opValue = opctx.re().getText();
+                            if (opctx.re().DOLLAR() != null) {
+                                opValue = replaceValues(opctx.re());
+                            }
+
+                            res.add(0, opValue);
+                        }
+                        opctx = opctx.opparamlist();
+                    }
+                    int i = 0;
+                    String re = bindings.get(ctx.qid().getText());
+                    while (re.contains("{" + i + "}")) {
+                        if (res.size() <= i) {
+                            break;
+                        }
+                        re = re.replace("{" + i + "}", res.get(i));
+                        i++;
+                    }
+
+                    value = re;
+                }
+            }
+        }
+
+        return value;
+    }
+
+    @Override
+    public void exitMacrodecl(PoCoParser.MacrodeclContext ctx) {
+        if(ctx.re() != null) {
+            if (bindings.containsKey(ctx.id().getText())) {
+                bindings.remove(ctx.id().getText());
+            }
+            String value = replaceBinding(ctx.re());
+            if(ctx.LPAREN() != null) {
+                PoCoParser.IdlistContext idctx = ctx.idlist();
+                List<String> ids = new ArrayList<String>();
+                while (idctx != null) {
+                    if (idctx.id() != null) {
+                        ids.add(0, idctx.id().getText());
+                    }
+                    idctx = idctx.idlist();
+                }
+
+                Iterator<String> itr = ids.iterator();
+                int i = 0;
+                while (itr.hasNext()) {
+                    value = value.replace("$" + itr.next(), "{" + i + "}");
+                    i++;
+                }
+            }
+            bindings.put(ctx.id().getText(), value);
+        }
+    }
+
+    @Override
+    public void exitSrecase(PoCoParser.SrecaseContext ctx) {
+        if(ctx.AT() != null)
+        {
+            //standard macro (not function)
+            if(bindings.containsKey(ctx.id().getText()))
+            {
+                bindings.remove(ctx.id().getText());
+            }
+            bindings.put(ctx.id().getText(), ctx.re().getText());
+        }
+    }
+
+    @Override
+    public void exitMatch(PoCoParser.MatchContext ctx) {
+        if(ctx.AT() != null)
+        {
+            //standard macro (not function)
+            if(bindings.containsKey(ctx.id().getText()))
+            {
+                bindings.remove(ctx.id().getText());
+            }
+            bindings.put(ctx.id().getText(), ctx.re().getText());
+        }
+    }
+
+    @Override
+    public void exitRe(PoCoParser.ReContext ctx) {
+        if(ctx.AT() != null)
+        {
+            //standard macro (not function)
+            if(bindings.containsKey(ctx.id().getText()))
+            {
+                bindings.remove(ctx.id().getText());
+            }
+            bindings.put(ctx.id().getText(), ctx.re().get(0).getText());
+        }
+    }
+
+    private String replaceValues(PoCoParser.ReContext rectx)
+    {
+        String re = rectx.getText();
+        if(rectx.DOLLAR() == null)
+        {
+            return re;
+        }
+        if(bindings.containsKey(rectx.qid().getText()))
+        {
+            String replace = "$" + rectx.qid().getText();
+            String value = bindings.get(rectx.qid().getText());
+
+            PoCoParser.OpparamlistContext opctx = rectx.opparamlist();
+            List<String> res = new ArrayList<String>();
+            while(opctx != null)
+            {
+                if(opctx.re() != null)
+                {
+                    String opValue = opctx.re().getText();
+                    if(opctx.re().DOLLAR() != null)
+                    {
+                        opValue = replaceValues(opctx.re());
+                    }
+
+                    res.add(0, opValue);
+                }
+                opctx = opctx.opparamlist();
+            }
+            int i = 0;
+            while (value.contains("{" + i + "}")) {
+                if(res.size() <= i)
+                {
+                    break;
+                }
+                value = value.replace("{" + i + "}", res.get(i));
+                i++;
+            }
+            re = re.replace(replace, value);
+        }
+        return re;
     }
 }
