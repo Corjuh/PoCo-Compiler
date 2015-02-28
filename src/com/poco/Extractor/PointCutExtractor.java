9c882877bbe5edd21c23bce5929bf6900c43b71e
Yan <yalbrigh@mail.usf.edu>
2015-02-28 15:49:51 -0500

    Update PointCutExtractor.java

--- src/com/poco/Extractor/PointCutExtractor.java
+++ src/com/poco/Extractor/PointCutExtractor.java
@@ -5,29 +5,23 @@ import com.poco.PoCoParser.PoCoParserBaseVisitor;
 import org.antlr.v4.runtime.misc.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by caoyan on 11/7/14.
  */
 public class PointCutExtractor extends PoCoParserBaseVisitor<Void> {
-    private LinkedHashSet<LinkedHashSet<String>> nodes = new LinkedHashSet<LinkedHashSet<String>>();
+    private LinkedHashSet<String> nodes = new LinkedHashSet<String>();
     //use this to distinguish the result from the action for generating advices
-    private LinkedHashSet<LinkedHashSet<String>> nodes4Results = new LinkedHashSet<LinkedHashSet<String>>();
-
-    private LinkedHashSet<String> pointcutStrings = new LinkedHashSet<>();
+    private LinkedHashSet<String> nodes4Results = new LinkedHashSet<String>();
     private String pointcutStr;
-
     private Closure closure;
-
     private boolean ptFromOpparamlist = false;
-
-    private boolean loadfmClosure = false;
-    private ArrayList<String> clousreAl = null;
-
     private boolean isResult = false;
-
 
     public PointCutExtractor(Closure closure) {
         this.closure = closure;
@@ -45,139 +39,116 @@ public class PointCutExtractor extends PoCoParserBaseVisitor<Void> {
                 pointcutStr = pointcutStr + " * (..); ";
             } else if (ctx.qid() != null) {
                 /**load from closure */
-                clousreAl = new ArrayList<String>();
-                loadFromClosure(ctx.qid().getText());
-                if (clousreAl != null && clousreAl.size() > 0) {
-                    for (Iterator<String> it = clousreAl.iterator(); it.hasNext(); ) {
-                        pointcutStr = pointcutStr + it.next();
+                if(closure != null && getClosureStr(ctx.qid().getText(), 1) != null) {
+                    pointcutStr = "";
+                    if (getClosureReStr(ctx.qid().getText()) != null) {
+                        visitRe(getClosureReStr(ctx.qid().getText()));
+                    } else
+                        //pointcutStr += "$"+ str + "$";
+                        pointcutStr += "$$" + ctx.qid().getText();
+                    if (ctx.opparamlist() != null) {
+                        if (ctx.opparamlist().getText().equals("%")) {
+                            pointcutStr += "(..)";
+                            add2NodesNodes4Result(pointcutStr);
+                        } else {
+                            ptFromOpparamlist = true;
+                            visitChildren(ctx);
+                            ptFromOpparamlist = false;
+                        }
+                    } else {
+                        add2NodesNodes4Result(pointcutStr);
                     }
-                } else
-                    pointcutStr = pointcutStr + ctx.qid().getText();
-                if (ctx.opparamlist() != null) {
-                    ptFromOpparamlist = true;
-                    visitChildren(ctx);
-                    ptFromOpparamlist = false;
                 } else {
-                    pointcutStr = pointcutStr + "(..)";
-                    pointcutStrings.add(pointcutStr);
-                    add2NodesNodes4Result(pointcutStrings);
-//                    nodes.add(pointcutStrings);
-                    pointcutStrings = new LinkedHashSet<String>();
+                    pointcutStr = pointcutStr + ctx.qid().getText();
+                    if (ctx.opparamlist() != null) {
+                        if (ctx.opparamlist().getText().equals("%")) {
+                            pointcutStr += "(..)";
+                            add2NodesNodes4Result(pointcutStr);
+                        } else {
+                            ptFromOpparamlist = true;
+                            visitChildren(ctx);
+                            ptFromOpparamlist = false;
+                        }
+                    } else {
+                        pointcutStr = pointcutStr + "(..)";
+                        add2NodesNodes4Result(pointcutStr);
+                    }
                 }
             } else if (ctx.function() != null) {
                 pointcutStr = pointcutStr + ctx.function().fxnname().getText();
                 if (ctx.function().INIT() != null) {
-                    pointcutStr = pointcutStr + "new";
+                    pointcutStr = ctx.function().fxnname().getText() + "new";
                 }
                 if (ctx.function().arglist() != null) {
-                    ptFromOpparamlist = true;
-                    visitChildren(ctx);
-                    ptFromOpparamlist = false;
+                    if(ctx.function().arglist().getText().length()==0) {
+                        pointcutStr += "()";
+                    }else {
+                        ptFromOpparamlist = true;
+                        visitChildren(ctx);
+                        ptFromOpparamlist = false;
+                    }
                 }
-                pointcutStrings.add(pointcutStr);
-                add2NodesNodes4Result(pointcutStrings);
-//                nodes.add(pointcutStrings);
-                pointcutStrings = new LinkedHashSet<String>();
+                add2NodesNodes4Result(pointcutStr);
             } else if (ctx.object() != null) {
-                pointcutStr = pointcutStr + ctx.object().qid().getText();
-                if (ctx.object().fieldlist() != null) {
-                    pointcutStr = pointcutStr + "(" + ctx.object().fieldlist().getText() + ")";
-                    pointcutStrings.add(pointcutStr);
-                    add2NodesNodes4Result(pointcutStrings);
-//                    nodes.add(pointcutStrings);
-                    pointcutStrings = new LinkedHashSet<String>();
-                } else if (ctx.re() != null) {
-                    pointcutStr = ctx.object().re().getText() + "(..)";
-                    pointcutStrings.add(pointcutStr);
-                    add2NodesNodes4Result(pointcutStrings);
-//                    nodes.add(pointcutStrings);
-                    pointcutStrings = new LinkedHashSet<String>();
-                }
+                if (ctx.object().POUND() != null)
+                    pointcutStr = ctx.object().re().getText();
+                else if (ctx.object().qid() != null)
+                    pointcutStr = pointcutStr + ctx.object().qid().getText();
             } else if (ctx.AT() != null) {
-                if (ctx.re(0) != null) {
-                    visitRe(ctx.re(0));
-                }
+                visitChildren(ctx);
             } else {
-                pointcutStr = " * " + ctx.getText() + "(..)";
-                pointcutStrings.add(pointcutStr);
-                add2NodesNodes4Result(pointcutStrings);
-//                nodes.add(pointcutStrings);
-                pointcutStrings = new LinkedHashSet<String>();
+                visitChildren(ctx);
             }
-
         } else {  //ptFromOpparamlist = true
             if (ctx.rewild() != null) {
-                //need fix later, not sure how to handle #String{%.class}
-                if (loadfmClosure == true)
-                    clousreAl.add(pointcutStr + "( .. )");
-                else {
-                    pointcutStr = pointcutStr + "( .. )";
-                }
+                pointcutStr = pointcutStr + "(..)";
             } else if (ctx.qid() != null) {
-                clousreAl = new ArrayList<String>();
-                loadFromClosure(ctx.qid().getText());
-                if (clousreAl != null && clousreAl.size() > 0) {
-                    String currentPCstr = pointcutStr;
-                    for (Iterator<String> it = clousreAl.iterator(); it.hasNext(); ) {
-                        pointcutStr = currentPCstr + it.next();
-                        pointcutStrings.add(pointcutStr);
-                    }
-                } else {
-                    pointcutStr = pointcutStr + "(" + ctx.qid().getText() + ") ";
-                    pointcutStrings.add(pointcutStr);
-                }
-                add2NodesNodes4Result(pointcutStrings);
-//                nodes.add(pointcutStrings);
-                pointcutStrings = new LinkedHashSet<String>();
+                if (closure != null && getClosureStr(ctx.qid().getText(), 0) != null) {
+                    if (closure.getContext(ctx.qid().getText()) != null)
+                        pointcutStr += "(" + getClosureStr(ctx.qid().getText(), 0) + "$$" + closure.getContext(ctx.qid().getText()).replace("\\", "") + "" +
+                                "$$) ";
+                    else
+                        pointcutStr += "(" + getClosureStr(ctx.qid().getText(), 0) + "$$" + ctx.qid().getText() + ") ";
+                } else
+                    pointcutStr = pointcutStr + "(" + ctx.qid().getText() + ")";
+                //add2NodesNodes4Result(pointcutStr);
             } else if (ctx.AT() != null) {
-                if (loadfmClosure == true)
-                    clousreAl.add(pointcutStr + "(" + ctx.re(0).getText() + ")");
-                else {
-                    pointcutStr = pointcutStr + "(" + ctx.re(0).getText() + ")";
-                }
+                //pointcutStr = pointcutStr + "(" + ctx.re(0).getText() + ")";
+                pointcutStr = pointcutStr + "(" + getClosureStr(ctx.id().getText(), 0) + ")";
+                //add2NodesNodes4Result(+pointcutStr);
             } else if (ctx.object() != null) {
                 if (ctx.object().POUND() != null) {
-                    if (ctx.object().qid().getText().equals("Integer")) {
-                        if (ctx.object().re() != null)
-                            visitRe(ctx.object().re());
-                    } else if (ctx.object().qid().getText().equals("String")) {
-                        if (ctx.object().re() != null)
-                            visitRe(ctx.object().re());
-                    }
-                    /* else { //fieldList
-                        if (loadfmClosure == true)
-                            clousreAl.add(pointcutStr + "(" + ctx.object().qid().getText() + ")");
+                    //format as #qid{re}
+                    pointcutStr = pointcutStr + "(" + ctx.object().qid().getText();
+                    String reStr = ctx.object().re().getText();
+                    //if reStr does not contain the "$", means the value is static,
+                    //so we can check it statically, otherwise we have to check it dynamically
+                    if (!reStr.contains("$")) {
+                        if (reStr.equals("%"))
+                            pointcutStr += "$$*.*$$";
                         else
-                            pointcutStr = pointcutStr + "(" + ctx.object().qid().getText() + ")";
-                    }*/
+                            pointcutStr += "$$" + reStr.replaceAll("%", "*") + "$$";
+                    } else { //so the value will be dynamic
+                        pointcutStr += reStr;
+                    }
+                    pointcutStr += ")";
                 } else if (ctx.id() != null) {
-                    if (loadfmClosure == true)
-                        clousreAl.add("(" + ctx.id().getText() + ") ");
-                    else
-                        pointcutStr = pointcutStr + "(" + ctx.id().getText() + ") ";
+                    pointcutStr = pointcutStr + "(" + ctx.id().getText() + ")";
                 } else {  //Null case
-                    if (loadfmClosure == true)
-                        clousreAl.add("(..) ");
-                    else
-                        pointcutStr = pointcutStr + "(..) ";
+                    pointcutStr = pointcutStr + "(..)";
                 }
+
             } else if (ctx.function() != null) {
                 //TODO: add detail later
-                if (loadfmClosure == true)
-                    clousreAl.add("(" + ctx.function().fxnname().getText() + ctx.function().arglist().getText() + ")");
-                else
-                    pointcutStr = pointcutStr + ctx.function().fxnname().getText();
+                pointcutStr = pointcutStr + ctx.function().fxnname().getText();
             } else if (ctx.rebop() != null) {
                 visitRe(ctx.re(0));
                 visitRe(ctx.re(1));
             } else {
-                if (loadfmClosure == true)
-                    clousreAl.add("(" + ctx.getText() + ") ");
-                else
-                    pointcutStr = pointcutStr + "(" + ctx.getText() + ") ";
+                if(ctx.getText().trim().length() >0)
+                    pointcutStr = pointcutStr + "(" + ctx.getText() + ")";
             }
         }
-
         return null;
     }
 
@@ -185,7 +156,6 @@ public class PointCutExtractor extends PoCoParserBaseVisitor<Void> {
     public Void visitExecution(@NotNull PoCoParser.ExecutionContext ctx) {
         if (ctx.map() != null) {
             visitSre(ctx.map().sre());
-            pointcutStrings = new LinkedHashSet<String>();
             visitExecution(ctx.map().execution());
         } else {
             visitChildren(ctx);
@@ -196,7 +166,6 @@ public class PointCutExtractor extends PoCoParserBaseVisitor<Void> {
     @Override
     public Void visitExch(@NotNull PoCoParser.ExchContext ctx) {
         pointcutStr = null;
-        pointcutStrings = new LinkedHashSet<String>();
         if (ctx.INPUTWILD() != null) {
             //just need monitor the action on the RHS of =>
             visitSre(ctx.sre());
@@ -252,70 +221,104 @@ public class PointCutExtractor extends PoCoParserBaseVisitor<Void> {
         }
         if (ctx.qid() != null) {
             pointcutStr = " * ";
-            clousreAl = new ArrayList<String>();
-            loadFromClosure(ctx.qid().getText());
-            if (clousreAl != null && clousreAl.size() > 0) {
-                String currentstr = pointcutStr;
-                pointcutStr = "";
-                for (Iterator<String> it = clousreAl.iterator(); it.hasNext(); ) {
-                    pointcutStr = currentstr + it.next() + "(..) ";
-                    pointcutStrings.add(pointcutStr);
-                }
-            } else {
-                pointcutStr = pointcutStr + ctx.qid().getText() + "(..)";
-                pointcutStrings.add(pointcutStr);
-            }
-            add2NodesNodes4Result(pointcutStrings);
-//            nodes.add(pointcutStrings);
-            pointcutStrings = new LinkedHashSet<String>();
+            String str = getClosureStr(ctx.qid().getText(), 1);
+            pointcutStr = pointcutStr + ctx.qid().getText() + "(..)";
+            add2NodesNodes4Result(pointcutStr);
         } else if (ctx.srebop() != null) {
-
             visitChildren(ctx.sre(0));
-            pointcutStrings.add(pointcutStr);
-            //nodes.add(pointcutStrings);
-            add2NodesNodes4Result(pointcutStrings);
-            pointcutStrings = new LinkedHashSet<String>();
-
+            add2NodesNodes4Result(pointcutStr);
             visitChildren(ctx.sre(1));
-            pointcutStrings.add(pointcutStr);
-            //nodes.add(pointcutStrings);
-            add2NodesNodes4Result(pointcutStrings);
-            pointcutStrings = new LinkedHashSet<String>();
-        } else {
+            add2NodesNodes4Result(pointcutStr);
+        }  else if (ctx.sreuop() != null) {
+            visitChildren(ctx.sre(0));
+            add2NodesNodes4Result(pointcutStr);
+        }
+        else {
             visitChildren(ctx);
         }
         return null;
     }
 
-    public LinkedHashSet<LinkedHashSet<String>> getPCStrings() {
-        return new LinkedHashSet<LinkedHashSet<String>>(nodes);
-    }
-    public LinkedHashSet<LinkedHashSet<String>> getPCStrs4Results() {
-        return new LinkedHashSet<LinkedHashSet<String>>(nodes4Results);
+    public LinkedHashSet<String> getPCStrings() {
+        return new LinkedHashSet<String>(nodes);
     }
 
-    public void loadFromClosure(String varName) {
-        if (closure != null)
-            if (closure.loadClosure(varName) != null) {
-                if (closure.loadClosure(varName).getVarType() == VarTypeVal.ClosureType.RE_TYPE &&
-                        closure.loadClosure(varName).getReContext() != null) {
-                    loadfmClosure = true;
-                    visitRe(closure.loadClosure(varName).getReContext());
-                } else if (closure.loadClosure(varName).getVarType() == VarTypeVal.ClosureType.SRE_TYPE &&
-                        closure.loadClosure(varName).getSreContext() != null) {
-                    loadfmClosure = true;
-                    visitSre(closure.loadClosure(varName).getSreContext());
-                }
+    public LinkedHashSet<String> getPCStrs4Results() {
+        return new LinkedHashSet<String>(nodes4Results);
+    }
+
+    public String getClosureStr(String varName, int type) {
+        if (closure != null && closure.loadClosure(varName) != null) {
+            if (type == 0)
+                return closure.loadClosure(varName).getVarType();
+            else if (type == 1) {
+                if (closure.loadClosure(varName).getVarLink() != null)
+                    return closure.loadClosure(varName).getVarLink().getText();
+                else
+                    return closure.loadClosure(varName).getVarContext();
             }
-        loadfmClosure = false;
+        }
+        return null;
     }
 
-    public void add2NodesNodes4Result(LinkedHashSet<String> pointcutStrs) {
+    public PoCoParser.ReContext getClosureReStr(String varName) {
+        return closure.loadClosure(varName).getVarLink();
+    }
+
+    public void add2NodesNodes4Result(String pointcutStr) {
         if (isResult) {
             //if it is result, move the pointcut from the action
-            nodes.remove(pointcutStrs);
-            nodes4Results.add(pointcutStrs);
-        }else
-            nodes.add(pointcutStrs);
+            nodes.remove(pointcutStr);
+            nodes4Results.add(pointcutStr);
+        } else {
+            if(!nodes4Results.contains(pointcutStr))
+                nodes.add(pointcutStr);
+        }
+    }
+
+    private String up8QidVal(String str) {
+        String returnStr = "";
+        int index = str.indexOf("$");
+        while (index != -1) {
+            int breakIndex = splitStr(str);
+            String qidval = str.substring(index + 1, breakIndex);
+            if (closure.isContains(qidval)) {
+                VarTypeVal val = closure.loadClosure(qidval);
+                if (val != null && val.getVarContext() != null) {
+                    if (index - 1 >= 0)
+                        returnStr += str.substring(0, index - 1);
+                    returnStr += val.getVarContext();
+                } else
+                    returnStr += str.substring(0, breakIndex + 1);
+            } else {
+                returnStr += str.substring(0, index) + str.substring(index + 1, breakIndex + 1);
+            }
+            if (breakIndex + 1 == str.length()) {
+                returnStr += str = str.substring(breakIndex, str.length());
+                break;
+            }
+            str = str.substring(breakIndex + 1, str.length());
+            index = str.indexOf("$");
+        }
+        return returnStr;
+    }
+
+    /**
+     * Split str by (,),. to find the $
+     *
+     * @param str
+     */
+    private static int splitStr(String str) {
+        int[] positions = new int[4];
+        positions[0] = str.indexOf("(");
+        positions[1] = str.indexOf(")");
+        positions[2] = str.indexOf(".");
+        positions[3] = str.indexOf(" ");
+
+        Arrays.sort(positions);
+        for (int index : positions) {
+            if (index != -1) return index;
+        }
+        return -1;
     }
 }
