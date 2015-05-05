042eeb9b0241a44f6567ac792498caf9b0e38f8b
Yan <yan@USF-Wifi-224-1-162.laptops.usf.edu>
2015-05-05 14:05:49 -0400

    fix alternation logic
    fix alternation logic, redundant incorrect logic is fixed

--- Runtime/src/com/poco/PoCoRuntime/AlternationExecution.java
+++ Runtime/src/com/poco/PoCoRuntime/AlternationExecution.java
@@ -1,47 +1,46 @@
-package com.poco;
-
+package com.poco.PoCoRuntime;
 
 /**
  * Created by caoyan on 12/4/14.
  */
-public class AlternationExecution extends SequentialExecution
-{
-    private Boolean resultBool = false;
-	private SRE resultSRE = null;
-	
-    public AlternationExecution(String modifier) throws PoCoException {
-        super(modifier);
-    }
- 	
-    @Override
-    public SRE query(Event event) {
-    	if (children.size() == 0){
+public class AlternationExecution extends SequentialExecution {
+	@Override
+	public String toString() {
+		return "AlternationExecution [currentCursor=" + currentCursor
+				+ ", children=" + children + ", isQueried=" + isQueried
+				+ ", resultBool=" + resultBool + ", resultSRE=" + resultSRE
+				+ "]";
+	}
+
+	public AlternationExecution(String modifier) throws PoCoException {
+		super(modifier);
+	}
+
+	@Override
+	public SRE query(Event event) {
+		if (children.size() == 0) {
 			return null;
 		}
-		if (!isQueried) {
-			if (children.get(0).accepts(event))  {
-				resultBool = true;
-				resultSRE = children.get(0).query(event);
-			} else  {
-				resultBool = children.get(1).accepts(event);
-				resultSRE = children.get(1).query(event);
-			}
-			isQueried = true;
-			return resultSRE;
+		if (children.get(0).accepts(event)) {
+			resultBool = true;
+			resultSRE = children.get(0).query(event);
+		} else {
+			resultBool = children.get(1).accepts(event);
+			resultSRE = children.get(1).query(event);
 		}
-		else {
-			return resultSRE;
-		}
-    }
+		return resultSRE;
+	}
 
-    @Override
-    public boolean accepts(Event event) {
-    	if (children.size() == 0) {
+	@Override
+	public boolean accepts(Event event) {
+		if (children.size() == 0) {
 			return false;
 		}
-		// The first child of a sequential execution must accept for its parent
-		// to accept
-		this.query(event);
-		return resultBool;
-    }
+		if(children.get(0).accepts(event)) {
+			return true;
+		}else {
+			boolean result =children.get(1).accepts(event);
+			return result;
+		}
+	}
 }
